0e3fdc174758747bbc561638e94e54dece2faf22
Haiqing Wang <haiqingwang1005@gmail.com>
2020-04-19 02:40:13 -0700

    Revise route order

--- api/server.js
+++ api/server.js
@@ -15,6 +15,17 @@ app.use(bodyParser.json());
 app.use('/api/shorten', shortenRoutes);
 app.use('/tiny', tinyRoutes);
 
+
+// Refer https://blog.bitsrc.io/react-production-deployment-part-3-heroku-316319744885
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    });
+}
+
 app.use((req, res, next) => {
     throw new HttpError('Could not find this route', 404);
 });
@@ -28,16 +39,6 @@ app.use((error, req, res, next) => {
     res.status(error.code || 500);
     res.json({message: error.message || 'An unknown error!'});
 });
-
-// Refer https://blog.bitsrc.io/react-production-deployment-part-3-heroku-316319744885
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../build', 'index.html'));
-    });
-}
 
 mongoose
     .connect(process.env.MONGODB_URI)
